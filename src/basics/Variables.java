4036a3310157f88e19ca305c4423d125fa767ef1
silvererudite <shamima.hossain19@gmail.com>
2023-06-24 16:35:26 +0600

    adding files

---
+++ src/basics/Variables.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Variables {
+    public static void main(String [] args){
+        // primitive variables
+        int animeId = 123;
+        String animeName = "Nana";
+        float animeRating = 9.12F;
+        char animeInitial = 'N';
+        System.out.println("Anime Id: " + animeId);
+        System.out.println("Anime name: " + animeName);
+        System.out.println("Anime rating: " + animeRating);
+        System.out.println("Anime initial: " + animeInitial);
+
+        // reference variables
+
+    }
+}
